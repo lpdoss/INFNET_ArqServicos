8607712f7f531094259c4494fbd95ebddcec17b3
Lucas Santos <lpsantos13@gmail.com>
2024-04-16 23:22:41 -0300

    updated remaining services

--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -24,12 +24,8 @@ builder.Services.AddHostedService<RabbitMQConsumer_ConfirmStockEvent>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
-
 
 app.UseAuthorization();
 
--- ShoppingCartService/Program.cs
+++ ShoppingCartService/Program.cs
@@ -24,11 +24,9 @@ builder.Services.AddHostedService<RabbitMQConsumer_DeleteCartEvent>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
