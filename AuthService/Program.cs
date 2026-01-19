2ff3b66a770b473e7db06636e43664fbd6c4c399
lpdoss <lpsantos13@gmail.com>
2026-01-18 21:53:33 -0500

    searching for conflicts

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddDbContext<AuthDbContext>(c => c.UseSqlServer(builderConfig["
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserService, UserService2>();
 
 var app = builder.Build();
 
